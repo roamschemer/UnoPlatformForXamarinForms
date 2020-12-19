ca7f9648978024111510c20b87170b60aeaa1461
roamschemer <roamschemer@gmail.com>
2020-12-20 01:19:18 +0900

    微調整

--- UnoPlatformForXamarinForms/Views/MainPage.xaml.cs
+++ UnoPlatformForXamarinForms/Views/MainPage.xaml.cs
@@ -12,7 +12,7 @@ namespace UnoPlatformForXamarinForms.Views {
     public partial class MainPage : ContentPage {
         public MainPage() {
             InitializeComponent();
-            var nameList = new List<string> { "1", "2", "3", "4", "5", "6" };
+            var nameList = new List<string> { "雨ヶ崎笑虹", "九条林檎", "白乃クロミ", "結目ユイ", "巻乃もなか" };
             BindingContext = new ViewModels.MainPageViewModel(this.Navigation, new Lottery(nameList) { Name = "Uno Platform WebAssembly Renderers for Xamarin.Forms" });
         }
     }
